8c73a93d404b00550820a9f760afd5228da482ca
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-07-21 15:15:16 +0300

    Initial commit of project.

---
+++ phone-factory-gateway/src/main/java/phone/phonefactorygateway/PhoneFactoryGatewayApplication.java
@@ -0,0 +1,13 @@
+package phone.phonefactorygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneFactoryGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneFactoryGatewayApplication.class, args);
+    }
+
+}
---
+++ phone-factory-gateway/src/test/java/phone/phonefactorygateway/PhoneFactoryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package phone.phonefactorygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneFactoryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
